dac3dc63f5583ce2e4e425fd29dac71ad4ee7097
Mahmoud Fathallah <212103670@o6u.edu.eg>
2025-06-01 23:43:47 +0300

    http://system

--- src/app/services/project.service.ts
+++ src/app/services/project.service.ts
@@ -7,7 +7,7 @@ import { Observable } from 'rxjs';
 })
 export class ProjectService {
   private http = inject(HttpClient);
-  private apiUrl = 'http://systemuniversity.runasp.net/api/Student';
+  private apiUrl = 'https://systemuniversity.runasp.net/api/Student';
 
   getQrCode(studentCode: number): Observable<Blob> {
     return this.http.get(`${this.apiUrl}/qrcode/${studentCode}`, {
