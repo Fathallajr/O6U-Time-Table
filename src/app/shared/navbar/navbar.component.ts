e0f97d1a23d79ff45ee82a5d7af1e1347ee3af56
Mahmoud Fathallah <212103670@o6u.edu.eg>
2025-06-02 01:23:06 +0300

    cur

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,21 +1,27 @@
-import { Component, inject } from '@angular/core'; 
-import { Router, RouterLink, RouterLinkActive } from '@angular/router';
-import { NgIf, AsyncPipe } from '@angular/common';
-import { AuthService } from '../../services/auth.service'; 
+import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [ RouterLink, RouterLinkActive, NgIf, AsyncPipe ],
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
-  authService = inject(AuthService);
-  private router = inject(Router); 
+  isMenuOpen = false;
 
-  logout(): void {
+  constructor(public authService: AuthService) {}
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  closeMenu() {
+    this.isMenuOpen = false;
+  }
+
+  logout() {
     this.authService.logout();
-    this.router.navigate(['/login']); 
+    this.closeMenu();
   }
 }
